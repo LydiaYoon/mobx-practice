d4a2dbf56f58e946c8fd31cfe4b4d66ebac6d72d
Lydia <44291638+LydiaYoon@users.noreply.github.com>
2020-06-22 14:34:05 +0900

    1.3 Decorate

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-import { observable, reaction, computed, autorun } from "mobx";
+import { decorate, observable, computed, autorun } from "mobx";
 
-// **** Observable State 만들기
-const calculator = observable({
-  a: 1,
-  b: 2
+class GS25 {
+  basket = [];
+
+  get total() {
+    console.log("계산중 ...");
+    // Reduce 함수로 배열 내부 객체의 price 총합 계산
+    return this.basket.reduce((prev, curr) => prev + curr.price, 0);
+  }
+
+  select(name, price) {
+    this.basket.push({ name, price });
+  }
+}
+
+// decorate를 통해서 각 값에 MobX 함수 적용
+decorate(GS25, {
+  basket: observable,
+  total: computed
 });
 
-// **** computed로 특정 값 캐싱
-const sum = computed(() => {
-  console.log("계산중 ... ");
-  return calculator.a + calculator.b;
-});
+const gs25 = new GS25();
+autorun(() => gs25.total);
 
-// **** autorun은 함수 내에서 조회하는 값을 자동으로 주시함
-autorun(() => console.log(`a 값이 ${calculator.a}로 바뀌었네요!`));
-autorun(() => console.log(`b 값이 ${calculator.b}로 바뀌었네요!`));
-autorun(() => sum.get());
-
-calculator.a = 10;
-calculator.b = 20;
-
-// 여러 번 조회해도 computed 안의 함수를 다시 호출하지 않지만..
-console.log(sum.value);
-console.log(sum.value);
-
-// 내부의 값이 바뀌면 다시 호출함
-calculator.a = 20;
-console.log(sum.value);
+gs25.select("물", 800);
+console.log(gs25.total);
+gs25.select("물", 800);
+console.log(gs25.total);
+gs25.select("포카칩", 1500);
+console.log(gs25.total);
