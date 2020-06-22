dac2e4ec6420f6e07571bd623cc50242e1426ac1
Lydia <44291638+LydiaYoon@users.noreply.github.com>
2020-06-22 14:41:43 +0900

    1.5 Decorate (without decorate func)

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
-import {
-  decorate,
-  observable,
-  computed,
-  autorun,
-  action,
-  transaction // *** transaction 불러옴
-} from "mobx";
+import { observable, computed, autorun, action, transaction } from "mobx";
 
 class GS25 {
+  @observable
   basket = [];
 
+  @computed
   get total() {
     console.log("계산중 ...");
     // Reduce 함수로 배열 내부 객체의 price 총합 계산
     return this.basket.reduce((prev, curr) => prev + curr.price, 0);
   }
 
+  @action
   select(name, price) {
     this.basket.push({ name, price });
   }
 }
-
-// decorate를 통해서 각 값에 MobX 함수 적용
-decorate(GS25, {
-  basket: observable,
-  total: computed,
-  select: action // **** 액션 명시
-});
 
 const gs25 = new GS25();
 autorun(() => gs25.total);
